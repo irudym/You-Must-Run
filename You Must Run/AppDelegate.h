ed058a948e77569ad72010b279982c1ce9860cd3
irudym <irudym@gmail.com>
2015-11-06 22:20:35 +0300

    Initial Commit

---
+++ You Must Run/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  You Must Run
+//
+//  Created by Igor on 06/11/15.
+//  Copyright © 2015 Igor Rudym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
